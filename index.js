44b2d74ffeb6aa209bb2ad6b0aa7ade1da5c69a3
Viktor Toth <Viktor.Toth@softwire.com>
2018-07-18 13:41:09 +0100

    Added list all function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require(`fs`);
 const parse = require('csv-parse/lib/sync');
 const moment = require(`moment`);
 
-
+const fileName = `Transactions2014.csv`;
 
 class Transaction {
     constructor(transactionDetails) {
@@ -11,28 +11,39 @@ class Transaction {
         this.from = transactionDetails[1];
         this.to = transactionDetails[2];
         this.narrative = transactionDetails[3];
-        this.ammount = transactionDetails[4];
+        this.ammount = convertMoneyStringToInt(transactionDetails[4]);
     }
 }
-
-console.log(`Please select mode: `);
-console.log(`(List All  or  List [Account])`);
-// const response = readLine.prompt();
-
-parseCSV();
-
 
 class User {
-        constructor(name) {
+    constructor(name) {
         this.name = name;
+        this.account = 0;
+    }
 
+    deduct(ammount) {
+        this.account -= ammount;
+    }
+
+    add(ammount) {
+        this.account += ammount;
     }
 }
 
 
-function parseCSV() {
+/*
+console.log(`Please select mode: `);
+console.log(`(List All  or  List [Account])`);
+const response = readLine.prompt();
+*/
+
+
+const tranactions = parseCSV(fileName);
+listAccounts(tranactions);
+
+function parseCSV(fileName) {
     // Reading csv file to string
-    const rawFile = fs.readFileSync(`Transactions2014.csv`, `utf8`);
+    const rawFile = fs.readFileSync(fileName, `utf8`);
 
     // Parsing to [[TRANSACTION-DETAILS], ...]
     const records = parse(rawFile);
@@ -50,6 +61,69 @@ function parseCSV() {
 
 
 
-function listAccount() {
+function listAccounts(transactions) {
 
+    // Storing all previously encountered users
+    const accounts = new Map();
+
+    // Going through every transaction
+    const len = transactions.length;
+    for (i = 1; i < len; i++) {
+
+        const current = transactions[i];
+
+        // Deduct from `from`
+        if (accounts.has(current.from)) {
+            accounts.get(current.from).deduct(current.ammount);
+        } else {
+            newUser = new User(current.from);
+            newUser.deduct(current.ammount);
+            accounts.set(current.from, newUser);
+        }
+
+        // Then add to `to`
+        if (accounts.has(current.to)) {
+            accounts.get(current.to).add(current.ammount);
+        } else {
+            newUser = new User(current.to);
+            newUser.add(current.ammount);
+            accounts.set(current.to, newUser);
+        }
+
+    }
+    
+    // List all accounts
+    accounts.forEach(function(value, key, map) {
+        const convertedAccount = convertMoneyIntToString(value.account);
+        console.log(`${value.name} has ${convertedAccount} in the bank`);
+    });
+}
+
+// Converts money from "xx.yy" to xxyy (int)
+function convertMoneyStringToInt(amt) {
+    const parts = amt.split(`.`);
+    if (parts.length === 2) {
+        if ((+parts[0]) < 0) {
+            return (+parts[0])*100 - (+parts[1]);
+        } else {
+            return (+parts[0])*100 + (+parts[1]);
+        }
+    } else {
+        return (+parts[0])*100;
+    }
+}
+
+
+// Converts money from xxyy (int) to "xx.yy"
+function convertMoneyIntToString(amt) {
+    if (amt < 0) {
+        amt = -amt;
+        const pennies = amt % 100;
+        const pounds = (amt - pennies) / 100;
+        return `-${pounds}.${pennies}`;
+    } else {
+        const pennies = amt % 100;
+        const pounds = (amt - pennies) / 100;
+        return `${pounds}.${pennies}`;
+    }
 }
