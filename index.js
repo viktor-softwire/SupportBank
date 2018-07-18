9d795825fce94ef732bea7a724d70c6acd5f5e96
Viktor Toth <Viktor.Toth@softwire.com>
2018-07-18 16:43:11 +0100

    Added logging functionality

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
-const readLine = require(`readline-sync`);
-const fs = require(`fs`);
+'use strict';
+
+const readLine = require('readline-sync');
+const fs = require('fs');
 const parse = require('csv-parse/lib/sync');
-const moment = require(`moment`);
+const moment = require('moment');
+const log4js = require('log4js');
+const logger = log4js.getLogger('logs\\debug.log');
 
-const fileName = `Transactions2014.csv`;
+const fileName = `DodgyTransactions2015.csv`;
 
+// Logging config
+log4js.configure({
+    appenders: {
+        file: { type: 'fileSync', filename: 'logs\\debug.log' }
+    },
+    categories: {
+        default: { appenders: ['file'], level: 'debug'}
+    }
+});
+
+// Each instance represents one tranaction (one line in file)
 class Transaction {
     constructor(transactionDetails) {
-        this.date = moment(transactionDetails[0], `DD/MM/YYYY`);
-        this.from = transactionDetails[1];
-        this.to = transactionDetails[2];
-        this.narrative = transactionDetails[3];
-        this.ammount = convertMoneyStringToInt(transactionDetails[4]);
+
+        if (transactionDetails.length !== 5) {
+            logger.fatal('Wrong number of parameters for Transaction constructor\n' +
+                                `(have ${transactionDetails.length} instead of 5)`)
+        }
+
+        const [dateString, from, to, narrative, amountStrig] = transactionDetails;
+
+        this.date = moment(dateString, 'DD/MM/YYYY');
+
+        if (!this.date.isValid()) {
+            logger.warn(`Invaild date (or wrong format): ${dateString}`);
+        }
+
+        this.from = from;
+        this.to = to;
+        this.narrative = narrative;
+        this.ammount = convertMoneyStringToInt(amountStrig);
     }
 }
 
+
+// Each instance represents one user, account = balance
 class User {
     constructor(name) {
+        logger.debug(`Created User instance ${name}`);
         this.name = name;
         this.account = 0;
     }
@@ -36,122 +67,193 @@ class User {
 
 }
 
+// Selecting operation mode
 
-console.log(`Please select mode: `);
-console.log(`(List All  or  List [Account])`);
+logger.debug('Application has started');
+
+console.log('Please select mode: ');
+console.log('(List All  or  List [Account])');
 const response = readLine.prompt();
 
-if (response === `List All`) {
-    const tranactions = parseCSV(fileName);
-    getAllUsers(tranactions);
-} else if (response.substring(0, 5) === `List `) {
+logger.debug(`User has entered operation mode: ${response}`);
+
+if (response === 'List All') {
+    logger.debug('User input has been matched to List All');
+    const transactions = parseCSV(fileName);
+    getAllUsers(transactions);
+
+} else if (response.substring(0, 5) === 'List ') {
+    logger.debug('User input has been mathced to List [NAME]');
     const userName = response.substring(5); 
-    const tranactions = parseCSV(fileName);
-    getUser(userName, tranactions);
+    logger.debug(`User has inputted name: ${userName}`);
+    const transactions = parseCSV(fileName);
+    getUser(userName, transactions);
+
 } else {
-    console.log(`Wrong mode of operation`);
+    logger.debug('User input has not been recognized');
+    console.log('Wrong mode of operation');
 }
 
 // Logs specific user's bank account
 function getUser(name, tranactions) {
+    logger.debug(`Getting account information on ${name}`);
     const users = calculateAccounts(tranactions);
     if (users.has(name)) {
+        logger.debug('User has been found');
         users.get(name).logAccount();
     } else {
-        console.log(`User not found`);
+        logger.debug('User has not been found');
+        console.log('User not found');
     }
 }
 
 
 
 // Logs all users' bank accounts
-function getAllUsers(tranactions) {
-    const users = calculateAccounts(tranactions);
+function getAllUsers(transactions) {
+    logger.debug(`Getting all users' info`);
+    const users = calculateAccounts(transactions);
     
-    users.forEach(function(value, key, map) {
+    logger.debug('Starting to print out users\' info');
+    users.forEach(function(value) {
         value.logAccount();
     });
 }
 
 function parseCSV(fileName) {
+
+    logger.debug(`Starting to read CSV file; file name: ${fileName}`);
+
     // Reading csv file to string
-    const rawFile = fs.readFileSync(fileName, `utf8`);
-
-    // Parsing to [[TRANSACTION-DETAILS], ...]
-    const records = parse(rawFile);
-
-    // Creating an array of tranactions
-    const len = records.length;
-    const transactions = new Array(len);
-    for (let i = 0; i < len; i++) {
-        transactions[i] = new Transaction(records[i]);
+    let rawFile;
+    try {
+        rawFile = fs.readFileSync(fileName, 'utf8');
+    } catch (err) {
+        logger.error(`Could not open ${fileName}; error message:`);
+        logger.error(`${err.message}`);
+        return null;
     }
 
-    return transactions;
+    // Parsing to [[TRANSACTION-DETAILS], ...]
+    logger.debug('Parsing string');
+    try {
+        const records = parse(rawFile);
+
+        // Creating an array of tranactions
+        const len = records.length;
+        const transactions = [];
+        logger.debug(`Length of transactions (including header): ${len}`);
+
+        // Iterating from 1 (first row is header)
+        for (let i = 1; i < len; i++) {
+            transactions[i] = new Transaction(records[i]);
+        }
+
+        logger.debug('Transactions have been successfully parsed');
+        return transactions;
+
+    } catch(err) {
+        logger.error(`Could not parse ${fileName} as CVS; error message:`);
+        logger.error(`${err.message}`);
+        return null;
+    }
 
 }
 
 // Returns the created accounts in a Map object
 function calculateAccounts(transactions) {
 
+    logger.debug('Started calculating accounts');
+
     // Storing all previously encountered users
     const accounts = new Map();
 
     // Going through every transaction
     const len = transactions.length;
-    for (i = 1; i < len; i++) {
+
+    // Starting iterating from 1 - first row is undefined (i.e. header)
+    for (let i = 1; i < len; i++) {
 
         const current = transactions[i];
+        logger.debug(`Started processing transaction ${current.from} -> ${current.to} £${convertMoneyIntToString(current.ammount)}\n` + 
+                        `@${current.date} message: ${current.narrative}`);
 
         // Deduct from `from`
         if (accounts.has(current.from)) {
+            logger.debug(`Deducting ${current.ammount} from ${current.from}`);
             accounts.get(current.from).deduct(current.ammount);
         } else {
-            newUser = new User(current.from);
+            logger.debug(`Adding new user ${current.from} to Map`);
+            const newUser = new User(current.from);
+            logger.debug(`Deducting ${current.ammount} from ${current.from}`);
             newUser.deduct(current.ammount);
             accounts.set(current.from, newUser);
         }
 
         // Then add to `to`
         if (accounts.has(current.to)) {
+            logger.debug(`Adding ${current.ammount} to ${current.to}`);
             accounts.get(current.to).add(current.ammount);
         } else {
-            newUser = new User(current.to);
+            logger.debug(`Adding new user ${current.to} to Map`);
+            const newUser = new User(current.to);
+            logger.debug(`Adding ${current.ammount} to ${current.to}`);
             newUser.add(current.ammount);
             accounts.set(current.to, newUser);
         }
 
     }
     
+    logger.debug('Transactions have been processed');
     // List all accounts
     return accounts;
 }
 
 // Converts money from "xx.yy" to xxyy (int)
 function convertMoneyStringToInt(amt) {
+    logger.debug(`Converting ${amt} to int`);
     const parts = amt.split(`.`);
-    if (parts.length === 2) {
-        if ((+parts[0]) < 0) {
-            return (+parts[0])*100 - (+parts[1]);
-        } else {
-            return (+parts[0])*100 + (+parts[1]);
-        }
-    } else {
-        return (+parts[0])*100;
+    
+    if (isNaN(amt)) {
+        logger.error(`Trying to convert non-money format: ${amt}`);
+        return null;
     }
+    
+    if ((parts.length === 0) || (parts.length > 2)) {
+        logger.error(`Invalid format: ${amt}`);
+    }
+
+    const pence = parts[1] || 0;
+    const sign = (+parts[0]) < 0 ? -1 : 1;
+    const result = (+parts[0])*100 + sign*(+pence);
+    logger.debug(`Result of conversion: ${result}`); 
+    return result;
 }
 
 
 // Converts money from xxyy (int) to "xx.yy"
 function convertMoneyIntToString(amt) {
-    if (amt < 0) {
-        amt = -amt;
-        const pennies = amt % 100;
-        const pounds = (amt - pennies) / 100;
-        return `-${pounds}.${pennies}`;
-    } else {
-        const pennies = amt % 100;
-        const pounds = (amt - pennies) / 100;
-        return `${pounds}.${pennies}`;
+    
+    logger.debug(`Converting ${amt} to string`);
+
+    if (!Number.isInteger(amt)) {
+        logger.error(`Trying to convert a non-integer number: ${amt}`);
+        return null;
     }
+
+
+    if (typeof(amt) !== 'number') {
+        logger.fatal('Trying to a convert not a number');
+    }
+
+    const sign = amt < 0 ? '-' : '';
+    const amtToUse = Math.abs(amt);
+
+
+    const pennies = amtToUse % 100;
+    const pounds = (amtToUse - pennies) / 100;
+    logger.debug(`Result of conversion: ${sign}${pounds}.${pennies}`);
+    return `${sign}${pounds}.${pennies}`;
+
+
 }
