820b71e0defa1abfca0bc119755c58c2f0e182c8
Viktor Toth <Viktor.Toth@softwire.com>
2018-07-19 16:57:32 +0100

    hotfix

--- loggerMessages.js
+++ loggerMessages.js
@@ -5,35 +5,27 @@ function logTrace(msg, logger) {
 }
 
 function logDebug(msg, logger) {
-    /*
     if (logger) {
         logger.debug(msg);
-    } */
-    logger.debug(msg)
+    }
 }
 
 function logWarn(msg, logger) {
-    /*
     if (logger) {
         logger.warn(msg);
-    } */
-    logger.warn(msg)
+    }
 }
 
 function logError(msg, logger) {
-    /*
     if (logger) {
         logger.error(msg);
-    } */
-    logger.error(msg);
+    }
 }
 
 function logFatal(msg, logger) {
-    /*
     if (logger) {
         logger.fatal(msg);
-    } */
-    logger.fatal(msg);
+    }
 }
 
 module.exports = {logTrace, logDebug, logWarn, logError, logFatal}
